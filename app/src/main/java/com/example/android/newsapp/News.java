adf72ef6eae9fa403adaab9328a1fa0953f2fac6
Oskar Hofmann <oskar.hofmann@gmx.de>
2018-05-27 15:24:30 +0200

    Updated layout, added QueryUtils and News class
    TODO: Update QueryUtils

---
+++ app/src/main/java/com/example/android/newsapp/News.java
@@ -0,0 +1,18 @@
+package com.example.android.newsapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * An {@Link News} contains information regarding a single news article
+ */
+class News {
+
+    private String mTitle;
+    private ArrayList<String> mAuthors;
+    private String mSection;
+    private Date mDate;
+    private String mArticleUrl;
+
+
+}
